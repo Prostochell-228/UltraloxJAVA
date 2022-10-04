35f22d26a0439d18c5eb49f61fca1100d2a35608
Yuri <114381954+Prostochell-228@users.noreply.github.com>
2022-10-04 19:04:35 +0700

    Add files via upload

---
+++ Loffet.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Loffet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        int n = scanner.nextInt();
+
+    }
+}
