6512232eb7618ddd2338e375d29a96183202461f
Yuri <114381954+Prostochell-228@users.noreply.github.com>
2023-02-09 16:09:16 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,58 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
 
-        double firstNumerator = scanner.nextDouble();
-        double firstDenomerator = scanner.nextDouble();
+    }
 
-        double secoondNumerator = scanner.nextDouble();
-        double secondDenomerator = scanner.nextDouble();
+    private void insertSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int currntElement = array[i];
 
-        System.out.println(
-                (firstNumerator/firstDenomerator) + (secoondNumerator/secondDenomerator)
-        );
-        System.out.println(
-                (firstNumerator/firstDenomerator) - (secoondNumerator/secondDenomerator)
-        );
-        System.out.println(
-                (firstNumerator/firstDenomerator) * (secoondNumerator/secondDenomerator)
-        );
-        Fraction firstFraction = new Fraction(firstNumerator, firstDenomerator);
-        Fraction secondFraction = new Fraction(secoondNumerator, secondDenomerator);
-        System.out.println(
-                Fractionhelp.calculateFraction(FractionCalculationAction.SUM, firstFraction, secondFraction)
-        );
+            int index = i - 1;
+            while (index >= 0 && array[index] > currntElement) {
+                array[index + 1] = array[index];
+                index--;
+            }
+            array[index + 1] = currntElement;
+        }
+    }
+    private  static void quikSort(int[]array,int low, int high){
+        if(array.length ==0){
+            return;
+        }
+        if (low>=high){
+            return;
+        }
+        int middleIndex = low +(high+low)/2;
+        int pivot = array[middleIndex];
 
+        int i = low;
+        int j = high;
+
+        while (i<=j){
+            while (array[i]>pivot){
+                i++;
+            }
+            while(array[j]>pivot){
+                j--;
+            }
+            if (i<=j){
+                int temp = array[i];
+                array[i]= array[j];
+                array[j]= temp;
+
+                i++;
+                j--;
+            }
+
+        }
+        if (low< j){
+            quikSort(array, low , j);
+        }
+        if (high> i){
+            quikSort(array,i, high);
+        }
     }
 }
